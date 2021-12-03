d3db9fa7a394ba2c297fe5d1d3a36eb032b2c361
Matias Moglia <matias.moglia@noname-it.com.ar>
2021-12-03 18:00:34 -0300

    new data

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,24 @@
 #   include apache
 class apache (
   String  $package_name,
+  String  $conf_file,
+  String  $template_file,
   Integer $port,
 ){
 
   package { $package_name:
     ensure => installed,
     alias  => 'package',
+  }
+
+  $config_content = epp("apache/${template_file}", { port => $port })
+
+  file { $conf_file:
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => $config_content,
   }
 
   service { $package_name:
