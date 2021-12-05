838608cb8c5148716d5a5f036eadbee6f1c950b0
Matias Moglia <matias.moglia@noname-it.com.ar>
2021-12-05 12:35:32 -0300

    new data

---
+++ lib/facter/processes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Facter.add(:apache_processes) do
+  context :kernel => 'Linux'
+  # https://puppet.com/docs/puppet/latest/fact_overview.html
+  setcode do
+    case Facter.value(:osfamily)
+    when 'Debian'
+      Facter::Core::Execution.execute('pgrep apache2 | wc -l')
+    when 'Red_Hat'
+      Facter::Core::Execution.execute('pgrep httpd | wc -l')
+    end
+  end
+end
