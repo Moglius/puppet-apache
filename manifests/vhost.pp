9d2913d032ef876059fa0933404ee51c6c114462
Matias Moglia <matias.moglia@noname-it.com.ar>
2021-12-05 09:52:00 -0300

    adding a defined_type vhost

---
+++ manifests/vhost.pp
@@ -0,0 +1,27 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   apache::vhost { 'namevar': }
+define apache::vhost (
+  Integer $port,
+  String  $vhost_name,
+  String  $document_root,
+) {
+
+  # root Directory
+  file { $document_root:
+    ensure => directory,
+    owner  => $apache::apache_user,
+    group  => $apache::apache_user,
+  }
+
+  file { "${document_root}/index.html":
+    ensure  => present,
+    owner   => $apache::apache_user,
+    group   => $apache::apache_user,
+    content => "Hello from ${facts.fqdn}"
+  }
+
+}
